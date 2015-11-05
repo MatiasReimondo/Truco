5428e30582a6d900e6160d6e41674621994c0f8a
Echemon <echemon@gmail.com>
2015-11-05 20:06:47 -0300

    Primer Commit, cambie los packages

--- src/truco/modelo/Carta.java
+++ src/truco/modelo/Carta.java
@@ -48,33 +48,33 @@ public class Carta {
 
     private void determinarFuerza(){
 
-        if (numero.equals(Numero.UNO)&& palo.equals(Palo.ESPADA)  ){fuerza= 14; }
+        if (numero.equals(Numero.UNO)&& palo.equals(Palo.ESPADA)  ){fuerza= 14; return;}
 
-        if (numero.equals(Numero.UNO)&& palo.equals(Palo.BASTO)  ){fuerza= 13; }
+        if (numero.equals(Numero.UNO)&& palo.equals(Palo.BASTO)  ){fuerza= 13; return;}
 
-        if (numero.equals(Numero.SIETE)&& palo.equals(Palo.ESPADA)  ){fuerza= 12; }
+        if (numero.equals(Numero.SIETE)&& palo.equals(Palo.ESPADA)  ){fuerza= 12; return;}
 
-        if (numero.equals(Numero.SIETE)&& palo.equals(Palo.ORO)  ){fuerza= 11; }
+        if (numero.equals(Numero.SIETE)&& palo.equals(Palo.ORO)  ){fuerza= 11; return;}
 
-        if (numero.equals(Numero.TRES)){fuerza= 10; }
+        if (numero.equals(Numero.TRES)){fuerza= 10; return;}
 
-        if (numero.equals(Numero.DOS) ){fuerza= 9; }
+        if (numero.equals(Numero.DOS) ){fuerza= 9; return;}
 
-        if (numero.equals(Numero.UNO)){fuerza= 8; }
+        if (numero.equals(Numero.UNO)){fuerza= 8; return;}
 
-        if (numero.equals(Numero.DOCE) ){fuerza= 7; }
+        if (numero.equals(Numero.DOCE) ){fuerza= 7; return;}
 
-        if (numero.equals(Numero.ONCE) ){fuerza= 6; }
+        if (numero.equals(Numero.ONCE) ){fuerza= 6; return;}
 
-        if (numero.equals(Numero.DIEZ) ){fuerza= 5; }
+        if (numero.equals(Numero.DIEZ) ){fuerza= 5; return;}
 
-        if (numero.equals(Numero.SIETE) ){fuerza= 4; }
+        if (numero.equals(Numero.SIETE) ){fuerza= 4; return;}
 
-        if (numero.equals(Numero.SEIS)){fuerza= 3; }
+        if (numero.equals(Numero.SEIS)){fuerza= 3; return;}
 
-        if (numero.equals(Numero.CINCO)){fuerza= 2; }
+        if (numero.equals(Numero.CINCO)){fuerza= 2; return;}
 
-        if (numero.equals(Numero.CUATRO)){fuerza= 1; }
+        if (numero.equals(Numero.CUATRO)){fuerza= 1; return;}
 
     }
 
