433c4f6af8a29fcaf66e52d469f62bf456374605
Echemon <echemon@gmail.com>
2015-11-12 19:36:34 -0300

    Algunos cambios a la Carta

--- src/truco/modelo/Carta.java
+++ src/truco/modelo/Carta.java
@@ -10,13 +10,12 @@ public class Carta {
     private Palo palo;
     private int numero;
     private int puntosEnvido;
-    private static Hashtable<Carta,Integer> tablaFuerza;
+    private static TablaFuerzaCarta tablaFuerza=new TablaFuerzaCarta();
 
     public Carta(int unNumero, Palo unPalo){
 
         this.palo = unPalo;
         this.numero= unNumero;
-        armarTablaDeFuerza();
     }
 
     public void setPalo(Palo palo) {
@@ -42,7 +41,7 @@ public class Carta {
 
     public int getFuerza(){
 
-        return tablaFuerza.get(this);
+        return tablaFuerza.getTabla().get(this);
     }
 
     public boolean equals(Carta carta) {
@@ -54,51 +53,5 @@ public class Carta {
 
     private void armarTablaDeFuerza(){
 
-        tablaFuerza=new Hashtable<>();
-
-        Carta AsDeEspada=new Carta(1,Palo.ESPADA);
-        tablaFuerza.put(AsDeEspada,14);
-
-        Carta AsDeBasto=new Carta(1,Palo.BASTO);
-        tablaFuerza.put(AsDeBasto,13);
-
-        Carta SieteDeEspada=new Carta(7,Palo.ESPADA);
-        tablaFuerza.put(SieteDeEspada,12);
-
-        Carta SieteDeOro=new Carta(7,Palo.ORO);
-        tablaFuerza.put(SieteDeOro,11);
-
-        Carta TresGenerico=new Carta(3,Palo.ORO);
-        tablaFuerza.put(TresGenerico,10);
-
-        Carta DosGenerico=new Carta(2,Palo.ESPADA);
-        tablaFuerza.put(DosGenerico,9);
-
-        Carta AsDeCopas=new Carta(1,Palo.COPA);
-        tablaFuerza.put(AsDeCopas,8);
-
-        Carta AsDeOro=new Carta(1,Palo.ORO);
-        tablaFuerza.put(AsDeOro,8);
-
-        Carta DoceGenerico=new Carta(12,Palo.ESPADA);
-        tablaFuerza.put(DoceGenerico,7);
-
-        Carta OnceGenerico=new Carta(11,Palo.ESPADA);
-        tablaFuerza.put(OnceGenerico,6);
-
-        Carta DiezGenerico=new Carta(10,Palo.ESPADA);
-        tablaFuerza.put(DiezGenerico,5);
-
-        Carta SieteDeCopas=new Carta(7,Palo.COPA);
-        tablaFuerza.put(SieteDeCopas,4);
-
-        Carta SeisGenerico=new Carta(6,Palo.COPA);
-        tablaFuerza.put(SeisGenerico,3);
-
-        Carta CincoGenerico=new Carta(5,Palo.COPA);
-        tablaFuerza.put(CincoGenerico,2);
-
-        Carta CuatroGenerico=new Carta(4,Palo.COPA);
-        tablaFuerza.put(CuatroGenerico,1);
     }
 }
