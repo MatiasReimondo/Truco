aaafed04c30d7c504ef1067255839295969c4dbc
Matias Reimondo <Matias Reimondo>
2015-11-12 21:53:27 -0300

    Tabla completa

--- src/truco/modelo/Carta.java
+++ src/truco/modelo/Carta.java
@@ -46,58 +46,146 @@ public class Carta {
         return this.puntosEnvido;
     }
 
-    private void armarTablaDeFuerza(){
+    private void armarTablaDeFuerza() {
 
-        tablaFuerza=new Hashtable<>();
+        tablaFuerza = new Hashtable<>();
 
-        Carta AsDeEspada=new Carta(1, Palo.ESPADA);
+        Carta AsDeEspada = new Carta(1, Palo.ESPADA);
         tablaFuerza.put(AsDeEspada, 14);
 
-        Carta AsDeBasto=new Carta(1,Palo.BASTO);
+        Carta AsDeBasto = new Carta(1, Palo.BASTO);
         tablaFuerza.put(AsDeBasto, 13);
 
-        Carta SieteDeEspada=new Carta(7,Palo.ESPADA);
+        Carta SieteDeEspada = new Carta(7, Palo.ESPADA);
         tablaFuerza.put(SieteDeEspada, 12);
 
-        Carta SieteDeOro=new Carta(7,Palo.ORO);
+        Carta SieteDeOro = new Carta(7, Palo.ORO);
         tablaFuerza.put(SieteDeOro, 11);
 
-        Carta TresGenerico=new Carta(3,Palo.ORO);
-        tablaFuerza.put(TresGenerico, 10);
+        //Los tres
+        Carta TresDeOro = new Carta(3, Palo.ORO);
+        tablaFuerza.put(TresDeOro, 10);
 
-        Carta DosGenerico=new Carta(2,Palo.ESPADA);
-        tablaFuerza.put(DosGenerico, 9);
+        Carta TresDeBasto = new Carta(3, Palo.BASTO);
+        tablaFuerza.put(TresDeBasto, 10);
 
-        Carta AsDeCopas=new Carta(1,Palo.COPA);
+        Carta TresDeEspada = new Carta(3, Palo.ESPADA);
+        tablaFuerza.put(TresDeEspada, 10);
+
+        Carta TresDeCopa = new Carta(3, Palo.COPA);
+        tablaFuerza.put(TresDeCopa, 10);
+
+        //Los dos
+        Carta DosDeOro = new Carta(2, Palo.ORO);
+        tablaFuerza.put(DosDeOro, 9);
+
+        Carta DosDeBasto = new Carta(2, Palo.BASTO);
+        tablaFuerza.put(DosDeBasto, 9);
+
+        Carta DosDeEspada = new Carta(2, Palo.ESPADA);
+        tablaFuerza.put(DosDeEspada, 9);
+
+        Carta DosDeCopa = new Carta(2, Palo.COPA);
+        tablaFuerza.put(DosDeCopa, 9);
+
+
+        //Ases
+        Carta AsDeCopas = new Carta(1, Palo.COPA);
         tablaFuerza.put(AsDeCopas, 8);
 
-        Carta AsDeOro=new Carta(1,Palo.ORO);
+        Carta AsDeOro = new Carta(1, Palo.ORO);
         tablaFuerza.put(AsDeOro, 8);
 
-        Carta DoceGenerico=new Carta(12,Palo.ESPADA);
-        tablaFuerza.put(DoceGenerico, 7);
+        //Doces
+        Carta DoceDeEspada = new Carta(12, Palo.ESPADA);
+        tablaFuerza.put(DoceDeEspada, 7);
 
-        Carta OnceGenerico=new Carta(11,Palo.ESPADA);
-        tablaFuerza.put(OnceGenerico, 6);
+        Carta DoceDeBasto = new Carta(12, Palo.BASTO);
+        tablaFuerza.put(DoceDeBasto, 7);
 
-        Carta DiezGenerico=new Carta(10,Palo.ESPADA);
-        tablaFuerza.put(DiezGenerico, 5);
+        Carta DoceDeOro = new Carta(12, Palo.ORO);
+        tablaFuerza.put(DoceDeOro, 7);
 
-        Carta SieteDeCopas=new Carta(7,Palo.COPA);
+        Carta DoceDeCopa = new Carta(12, Palo.COPA);
+        tablaFuerza.put(DoceDeCopa, 7);
+
+        //Onces
+        Carta OnceDeEspada = new Carta(11, Palo.ESPADA);
+        tablaFuerza.put(OnceDeEspada, 6);
+
+        Carta OnceDeBasto = new Carta(11, Palo.BASTO);
+        tablaFuerza.put(OnceDeBasto, 6);
+
+        Carta OnceDeOro = new Carta(11, Palo.ORO);
+        tablaFuerza.put(OnceDeOro, 6);
+
+        Carta OnceDeCopa = new Carta(11, Palo.COPA);
+        tablaFuerza.put(OnceDeCopa, 6);
+
+        //Diez
+        Carta DiezDeEspada = new Carta(10, Palo.ESPADA);
+        tablaFuerza.put(DiezDeEspada, 5);
+
+        Carta DiezDeBasto = new Carta(10, Palo.BASTO);
+        tablaFuerza.put(DiezDeBasto, 5);
+
+        Carta DiezDeOro = new Carta(10, Palo.ORO);
+        tablaFuerza.put(DiezDeOro, 5);
+
+        Carta DiezDeCopa = new Carta(10, Palo.COPA);
+        tablaFuerza.put(DiezDeCopa, 5);
+
+        //Siestes
+        Carta SieteDeBasto = new Carta(7, Palo.BASTO);
+        tablaFuerza.put(SieteDeBasto, 4);
+
+        Carta SieteDeCopas = new Carta(7, Palo.COPA);
         tablaFuerza.put(SieteDeCopas, 4);
 
-        Carta SeisGenerico=new Carta(6,Palo.COPA);
-        tablaFuerza.put(SeisGenerico, 3);
+        //Seis
+        Carta SeisDeEspada = new Carta(6, Palo.ESPADA);
+        tablaFuerza.put(SeisDeEspada, 3);
 
-        Carta CincoGenerico=new Carta(5,Palo.COPA);
-        tablaFuerza.put(CincoGenerico, 2);
+        Carta SeisDeBasto = new Carta(6, Palo.BASTO);
+        tablaFuerza.put(SeisDeBasto, 3);
 
-        Carta CuatroGenerico=new Carta(4,Palo.COPA);
-        tablaFuerza.put(CuatroGenerico, 1);
+        Carta SeisDeCopa = new Carta(6, Palo.COPA);
+        tablaFuerza.put(SeisDeCopa, 3);
 
+        Carta SeisDeOro = new Carta(6, Palo.ORO);
+        tablaFuerza.put(SeisDeOro, 3);
+
+        //Cinco
+        Carta CincoDeEspada = new Carta(5, Palo.ESPADA);
+        tablaFuerza.put(CincoDeEspada, 2);
+
+        Carta CincoDeBasto = new Carta(5, Palo.BASTO);
+        tablaFuerza.put(CincoDeBasto, 2);
+
+        Carta CincoDeCopa = new Carta(5, Palo.COPA);
+        tablaFuerza.put(CincoDeCopa, 2);
+
+        Carta CincoDeOro = new Carta(5, Palo.ORO);
+        tablaFuerza.put(CincoDeOro, 2);
+
+        //Cuatro
+        Carta CuatroDeEspada = new Carta(4, Palo.ESPADA);
+        tablaFuerza.put(CuatroDeEspada, 1);
+
+        Carta CuatroDeBasto = new Carta(4, Palo.BASTO);
+        tablaFuerza.put(CuatroDeBasto, 1);
+
+        Carta CuatroDeCopa = new Carta(4, Palo.COPA);
+        tablaFuerza.put(CuatroDeCopa, 1);
+
+        Carta CuatroDeOro = new Carta(4, Palo.ORO);
+        tablaFuerza.put(CuatroDeOro, 1);
     }
 
-    @Override
+
+
+
+        @Override
     public int hashCode() {
         int result = palo != null ? palo.hashCode() : 0;
         result = 31 * result + numero;
