13ae2fde315adf09748e7c0550d2a324df4af207
Echemon <echemon@gmail.com>
2015-11-14 03:48:31 -0300

    Tiene algunos errores porque falta completar

---
+++ test/integracion/ResolverJugadas.java
@@ -0,0 +1,48 @@
+package integracion;
+
+import org.junit.Before;
+import org.junit.Test;
+import truco.modelo.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResolverJugadas {
+
+    private Mesa mesa;
+    private Equipo equipoPepe;
+    private Equipo equipoJuan;
+    private Jugador jugadorPepe;
+    private Jugador jugadorJuan;
+    private List<Jugador> listaJugadores;
+
+    @Before
+    public void setup(){
+        mesa=new Mesa();
+        equipoPepe =new Equipo("EquipoPepe");
+        equipoJuan=new Equipo("EquipoJuan");
+        jugadorJuan=new Jugador("Juan");
+        jugadorPepe=new Jugador("Pepe");
+        equipoPepe.agregarIntegrante(jugadorPepe);
+        equipoJuan.agregarIntegrante(jugadorJuan);
+        listaJugadores=new ArrayList<>();
+        listaJugadores.add(jugadorPepe);
+        listaJugadores.add(jugadorJuan);
+        jugadorJuan.setMesa(mesa);
+        jugadorPepe.setMesa(mesa);
+    }
+
+    @Test
+    public void testCartaMasFuerteVenceACartaMasDebil(){
+
+
+        mesa.nuevaRonda();
+        jugadorPepe.robarCarta(new Carta(4,Palo.BASTO));
+        jugadorJuan.robarCarta(new Carta(5,Palo.BASTO));
+
+        jugadorPepe.jugarCarta(4,Palo.BASTO);
+        jugadorJuan.jugarCarta(5,Palo.BASTO);
+
+
+    }
+}
