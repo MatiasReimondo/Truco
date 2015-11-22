13dea2fe25adb71f2ddbbb46692dbec3769f461e
jessica3091 <jessica.3091@gmail.com>
2015-11-22 12:55:31 -0300

    Update TrucoNoCantado.java

--- src/truco/modelo/estadosTruco/TrucoNoCantado.java
+++ src/truco/modelo/estadosTruco/TrucoNoCantado.java
@@ -7,7 +7,7 @@ import truco.modelo.excepciones.NoSePuedeCantarAhoraException;
  *********************************************************************************/
 public class TrucoNoCantado implements EstadoTruco {
 
-    /********************** Métodos de la clase ***********************************/
+    /********************** Metodos de la clase ***********************************/
     @Override
     public int getPuntaje() {
         return 1;
